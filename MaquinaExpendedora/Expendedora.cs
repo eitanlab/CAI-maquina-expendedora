765abb6dd3be6cb0c1a297d3138e180b6b6298b2
Eitan Lab <eitanlab@gmail.com>
2020-09-21 01:24:15 -0300

    creacion de clases, menu de usuario

---
+++ MaquinaExpendedora/Expendedora.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaExpendedora
+{
+    public class Expendedora
+    {
+        List<Lata> _latas;
+        string _proveedor;
+        int _capacidad;
+        double _dinero;
+        bool _encendida;
+        public Expendedora()
+        {
+            List<Lata> lasLatas = new List<Lata>();
+            lasLatas.Add(new Lata("CO1"));
+            lasLatas.Add(new Lata("CO2"));
+            lasLatas.Add(new Lata("SP1"));
+            lasLatas.Add(new Lata("SP2"));
+            lasLatas.Add(new Lata("FA1"));
+            lasLatas.Add(new Lata("FA2"));
+            latas = lasLatas;
+        }
+
+        public List<Lata> latas
+        {
+            get
+            {
+                return _latas;
+            }
+            set
+            {
+                _latas = value;
+            }
+        }
+        public string proveedor
+        {
+            get
+            {
+                return _proveedor;
+            }
+            set
+            {
+                _proveedor = value;
+            }
+        }
+        public int capacidad
+        {
+            get
+            {
+                return _capacidad;
+            }
+            set
+            {
+                _capacidad = value;
+            }
+        }
+        public double dinero
+        {
+            get
+            {
+                return _dinero;
+            }
+            set
+            {
+                _dinero = value;
+            }
+        }
+        public bool encendida
+        {
+            get
+            {
+                return _encendida;
+            }
+            set
+            {
+                _encendida = value;
+            }
+        }
+        public void agregarLata(Lata codigo)
+        {
+
+        }
+        public Lata extraerLata(string a,double b)
+        {
+            return null;
+        }
+        public string getBalance() {
+            return "";
+        }
+        public void encenderMaquina()
+        {
+            if (encendida) throw new Exception();
+            encendida = true;
+        }
+        public bool estaVacia()
+        {
+            return false;
+        }
+
+        public string mostrarLatasDisponibles()
+        {
+            string strListadoDeLatas = "";
+            foreach (Lata lata in latas)
+            {
+                strListadoDeLatas += Environment.NewLine + lata.toString();
+            }
+            return strListadoDeLatas;
+        }
+
+        public bool existeCodigoLata(string codigo)
+        {
+            bool existeCodigo = false;
+            foreach(Lata lata in latas)
+            {
+                if (lata.codigo == codigo) existeCodigo = true;
+            }
+            return existeCodigo;
+        }
+
+    }
+}
---
+++ MaquinaExpendedora/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaExpendedora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expendedora expendedora1 = new Expendedora();
+            bool mostrarMenu = true;
+            while (mostrarMenu)
+            {
+                mostrarMenu = menuUsuario(expendedora1);
+            }
+        }
+        static bool menuUsuario(Expendedora expendedora)
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Encender máquina");
+            if (expendedora.encendida)
+            {
+                Console.WriteLine("2. Ver listado de latas disponibles");
+                Console.WriteLine("3. Insertar lata");
+                Console.WriteLine("4. Comprar lata");
+                Console.WriteLine("5. Ver balance de máquina");
+                Console.WriteLine("6. Ver stock y detalle por lata");
+                Console.WriteLine("Ingrese el número de operación:");
+            }
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    expendedora.encenderMaquina();
+                    return true;
+                case "2":
+                    Console.WriteLine(expendedora.mostrarLatasDisponibles());
+                    return true;
+                case "3":
+                    Console.WriteLine(expendedora.mostrarLatasDisponibles());
+                    Console.WriteLine("Ingrese código de lata a insertar");
+                    string codigoLataAIngresar = Console.ReadLine();
+                    if (expendedora.existeCodigoLata(codigoLataAIngresar))
+                    {
+                        Lata lataAIngresar = new Lata();
+                        lataAIngresar.codigo = codigoLataAIngresar;
+                        Console.Clear();
+                        Console.WriteLine("Ingrese el precio de venta");
+                        string precioLataAIngresar = Console.ReadLine();
+                        if (Convert.ToDouble(precioLataAIngresar) > 0) {
+                            lataAIngresar.precio = Convert.ToDouble(precioLataAIngresar);
+                            Console.Clear();
+                            Console.WriteLine("Ingrese el volumen de lata");
+                            string volumenLataAIngresar = Console.ReadLine();
+                            if (Convert.ToDouble(volumenLataAIngresar) > 0) {
+                                lataAIngresar.volumen = Convert.ToDouble(volumenLataAIngresar);
+                            }
+                        }
+                        return true;
+                    }
+                    return true;
+                default:
+                    return true;
+            }
+        }
+        static void ingresarLata(Expendedora expendedora) { 
+            
+        }
+        static void extraerLata(Expendedora expendedora) { }
+        static void obtenerBalance(Expendedora expendedora) { }
+        static void mostrarStock(Expendedora expendedora) { }
+    }
+}
